98c8e96c1a5d0a983861ee9dee2def724144a1a5
damokravetz <kravetzdamian@gmail.com>
2022-07-23 18:40:36 -0300

    primer commit

---
+++ src/main/java/com/redmond/ejercicioredmond/EjercicioRedmondApplication.java
@@ -0,0 +1,13 @@
+package com.redmond.ejercicioredmond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioRedmondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioRedmondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redmond/ejercicioredmond/EjercicioRedmondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmond.ejercicioredmond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioRedmondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

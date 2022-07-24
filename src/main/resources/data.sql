30cc7d12e7d8b26aa1288378a408615718b81acb
damokravetz <kravetzdamian@gmail.com>
2022-07-23 23:28:38 -0300

    test cambio version java para deploy db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO empleado (legajo, nombre, sueldo) VALUES (1234, 'Javier', 96462);
+INSERT INTO empleado (legajo, nombre, sueldo) VALUES (1235, 'Facundo', 132735);
+INSERT INTO empleado (legajo, nombre, sueldo) VALUES (1236, 'Dario', 113293);
